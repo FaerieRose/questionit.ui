a0e3daf8d148dfd9b20ffbf11a1425362a341a30
FaerieRose <rosalynn5772-git@yahoo.com>
2016-11-10 21:48:01 +0100

    QuestionService some error handling implemented

--- src/app/question/question.service.ts
+++ src/app/question/question.service.ts
@@ -16,17 +16,33 @@ export class QuestionService {
 	private options = new RequestOptions({ headers: this.headers });
 
 	private questionUrl = "http://localhost:8081/api/questions/";
+	private currentUrl: string;
 
   constructor(private http: Http) {}
 
+	// -------------------------------------------------------------
 	// GET one question with specific id
 	getQuestion(id) : Observable<Question>{
-		 return this.http.get(this.questionUrl + id).map((res: Response) => res.json());
+		this.currentUrl = this.questionUrl + id;
+		return this.http.get(this.currentUrl).map(this.getExtractData);
 	}
 
+	// -------------------------------------------------------------
 	// GET one question exam style with specific id
 	getQuestionExam(id) : Observable<Question>{
-		 return this.http.get(this.questionUrl + "exam/" + id).map((res: Response) => res.json());
+		this.currentUrl = this.questionUrl + "exam/" + id;
+		return this.http.get(this.currentUrl).map(this.getExtractData);
 	}
+
+	// -------------------------------------------------------------
+	// Returns the received JSON data if the response from the GET is 200, otherwise an empty JSON object
+	private getExtractData(res: Response) {
+		if (res.status == 200) {
+			return res.json();
+		} else {
+			console.error("Response from " + res.url + ": Status: " + res.status);
+			return {}
+		}
+  }
 
 }
--- src/app/question/question.ts
+++ src/app/question/question.ts
@@ -2,11 +2,13 @@
 /* Author       : FaerieRose                                                           */
 /* Date created : 10 Nov 2016                                                          */
 /* ----------------------------------------------------------------------------------- */
+import '../enums';
+
 export class Question {
   id? : number;
   name : string;
-  programmingLanguage: string;
-  forExam: string;
+  programmingLanguage: EnumLanguages;
+  forExam: EnumExams;
   question: string;
   explantionAnswer: string;
   typeOfQuestion: string;
