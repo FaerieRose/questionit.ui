443af5d9b6b6b89704493e2d3db0a59525e08bdb
FaerieRose <rosalynn5772-git@yahoo.com>
2016-11-14 16:30:34 +0100

    New version Question Template

--- src/app/enums.ts
+++ src/app/enums.ts
@@ -1,12 +1,15 @@
 export enum EnumLanguages {
   NONE,
   JAVA,
-  HTML_CSS_JS
+  HTML_CSS_JS,
+  PHP,
+  C_SHARP
 }
 
 export enum EnumExams {
   NONE,
   OCA,
   OCP,
-  _70_480
+  _70_480,
+  _70_483
 }
