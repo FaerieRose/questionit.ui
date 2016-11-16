fd67f4b3b4ad61136caa9036f714a6d24ab0618d
FaerieRose <rosalynn5772-git@yahoo.com>
2016-11-16 10:43:11 +0100

    Cleaning up the code

--- src/app/question/question-display.component.ts
+++ src/app/question/question-display.component.ts
@@ -1,39 +1,40 @@
-import { Component, Input, ViewChild, AfterViewInit } from '@angular/core';
-import {Observable} from 'rxjs/Rx';
+import { Component, ViewChild } from '@angular/core';
+import { Input, AfterViewInit } from '@angular/core';
+import {Observable}             from 'rxjs/Rx';
 
 @Component({
   selector: 'question-display',
-  template: `
-    <p #qstn [innerHTML]="_q"></p>
-  `
+  template: '<p #qstn [innerHTML]="_question"></p>'
 })
 export class QuestionDisplayComponent {
-  @ViewChild("qstn") qstn;
-  _q: string = 'test';
+  _question: string = 'test';
   pElement: HTMLParagraphElement;
 
+  @ViewChild("qstn") qstn;
   @Input() 
-  set q(q: string) {
-    this._q = q;
+  set question(question: string) {
+    this._question = question;
+    // During initialisation the pElement has not yet been created
     if(this.pElement != null) {
+      // Wait for the update of pElement before updating the CSS
       let timer = Observable.timer(500);
       timer.subscribe(t => {
         this.updateCSS();
       });
     }
   }
-  get q() {
-    return this._q;
+  get question() {
+    return this._question;
   }
 
-
+  // After the View has been initialised connect #qstn to pElement and update the CSS
   ngAfterViewInit() {
     this.pElement = this.qstn.nativeElement;
     this.updateCSS();
   }
 
+  // Update the CSS of the question
   updateCSS() {
-    console.log("childElementCount=" + this.pElement.childElementCount);
     for (let i=0; i<this.pElement.children.length ; i++) {
       let chld = this.pElement.children[i];
       console.log(i + " - tagName = " + chld.tagName);
