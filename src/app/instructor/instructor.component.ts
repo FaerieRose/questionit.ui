5bf646cc63a9c2d3bd678f05efd3095b240065fe
FaerieRose <rosalynn5772-git@yahoo.com>
2016-11-25 15:35:56 +0100

    Instructor.component updates in css, html and ts

--- src/app/instructor/instructor.component.ts
+++ src/app/instructor/instructor.component.ts
@@ -20,19 +20,12 @@ export class InstructorComponent implements OnInit {
     private globalService: GlobalService) {
 
     this.instructor = new Instructor();
-    // this.instructor.firstName = "remond";
     this.instructorService.getInstructorById(1).subscribe(instructor => this.instructor = instructor);
-  }
-
-  changeId($event) {
-    let id = $event.target.value;
-    this.getInstructorById(id);
   }
 
   ngOnInit() {
     this.getInstructorById(1);
     this.getInstructorList();
-    // this.instructor = this.globalService.getInstructorID();
   }
 
   getInstructorById(id: number) {
@@ -41,13 +34,16 @@ export class InstructorComponent implements OnInit {
       if (instructor.id == 1) {
         console.log("---- NEW INSTRUCTOR CREATED");
         this.instructor = new Instructor();
-
       } else {
         this.instructor = instructor;
       }
+    });
+  }
 
-
-
+  getInstructorList() {
+    this.instructorService.getInstructors().subscribe(instructors => {
+      this.instructorList = instructors;
+      console.log(this.instructorList.length);
     });
   }
 
@@ -59,28 +55,19 @@ export class InstructorComponent implements OnInit {
 
   saveInstructor() {
     let instr = this.instructor;
-
     this.instructorService.postNewInstructor(instr).subscribe(Instructor => {
       console.log("POST SUCCEEDED");
+      this.getInstructorList();
     });
   }
-  updateFirstName($event, i: number) { this.instructorList[i].firstName = $event.target.value; this.saveUpdatedInstructor(this.instructorList[i]) }
-  updateLastName($event, i: number) { this.instructorList[i].lastName = $event.target.value;this.saveUpdatedInstructor(this.instructorList[i]) }
-  updateEmail($event, i: number) { this.instructorList[i].email = $event.target.value; this.saveUpdatedInstructor(this.instructorList[i])}
-  updateID($event) { this.instructor.id = $event.target.value; }
-  updatevalid($event) { this.instructor.valid = $event.target.value; }
+
+  updateListFirstName($event, i: number) { this.instructorList[i].firstName = $event.target.value; this.saveUpdatedInstructor(this.instructorList[i]) }
+  updateListLastName($event, i: number)  { this.instructorList[i].lastName = $event.target.value;  this.saveUpdatedInstructor(this.instructorList[i]) }
+  updateListEmail($event, i: number)     { this.instructorList[i].email = $event.target.value;     this.saveUpdatedInstructor(this.instructorList[i]) }
+  updateFirstName($event) { this.instructor.firstName = $event.target.value; }
+  updateLastName($event)  { this.instructor.lastName = $event.target.value;  }
+  updateEmail($event)     { this.instructor.email = $event.target.value;     }
+  updatevalid($event)     { this.instructor.valid = $event.target.value;     }
 
 
-  showAllInstructors() {
-    var element = document.getElementById("instructorlist").appendChild(document.createElement("article"));
-    console.log("Element = " + element);
-  }
-  getInstructorList() {
-    this.instructorService.getInstructors().subscribe(instructors => {
-      this.instructorList = instructors;
-      console.log(this.instructorList.length);
-    });
-  }
 }
-
-
