16368c79b0ced9f3b8a9d29aa81752e0866e632b
RemondKarst <remond.karst@gmail.com>
2016-11-25 14:57:28 +0100

    editor en overview op 1 pagina

--- src/app/instructor/instructor.component.ts
+++ src/app/instructor/instructor.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit }  from '@angular/core';
-import { GlobalService }            from '../global.service';
-import { InstructorService }  from './instructor.service';
-import { Instructor }         from './instructor';
+import { Component, OnInit } from '@angular/core';
+import { GlobalService } from '../global.service';
+import { InstructorService } from './instructor.service';
+import { Instructor } from './instructor';
 
 
 @Component({
   selector: 'my-instructor',
   templateUrl: 'instructor.component.html',
-  styleUrls: [ 'instructor.component.css' ],
-  providers: [ InstructorService ]
+  styleUrls: ['instructor.component.css'],
+  providers: [InstructorService]
 
 })
 export class InstructorComponent implements OnInit {
@@ -16,15 +16,15 @@ export class InstructorComponent implements OnInit {
   instructorList: Instructor[];
 
   constructor(
-          private instructorService: InstructorService,
-          private globalService    : GlobalService) {
+    private instructorService: InstructorService,
+    private globalService: GlobalService) {
 
     this.instructor = new Instructor();
- // this.instructor.firstName = "remond";
+    // this.instructor.firstName = "remond";
     this.instructorService.getInstructorById(1).subscribe(instructor => this.instructor = instructor);
   }
 
- changeId($event) {
+  changeId($event) {
     let id = $event.target.value;
     this.getInstructorById(id);
   }
@@ -32,17 +32,17 @@ export class InstructorComponent implements OnInit {
   ngOnInit() {
     this.getInstructorById(1);
     this.getInstructorList();
-   // this.instructor = this.globalService.getInstructorID();
+    // this.instructor = this.globalService.getInstructorID();
   }
 
- getInstructorById(id: number) {
- //   this.instructor = null;
+  getInstructorById(id: number) {
+    //   this.instructor = null;
     this.instructorService.getInstructorById(id).subscribe(instructor => {
-      if (instructor.id==1){
+      if (instructor.id == 1) {
         console.log("---- NEW INSTRUCTOR CREATED");
         this.instructor = new Instructor();
-        
-      }else {
+
+      } else {
         this.instructor = instructor;
       }
 
@@ -50,30 +50,37 @@ export class InstructorComponent implements OnInit {
 
     });
   }
+
+  saveUpdatedInstructor(instr: Instructor) {
+    this.instructorService.postNewInstructor(instr).subscribe(Instructor => {
+      console.log("POST SUCCEEDED");
+    });
+  }
+
   saveInstructor() {
     let instr = this.instructor;
 
-        this.instructorService.postNewInstructor(instr).subscribe(Instructor => {
-          console.log("POST SUCCEEDED");
-        });
-      }
-   updateFirstName($event, i:number) { this.getInstructorById(i); this.instructor.firstName = $event.target.value; }
-   updateLastName($event) { this.instructor.lastName = $event.target.value; }
-   updateEmail($event) { this.instructor.email = $event.target.value; }
-   updateID($event) { this.instructor.id = $event.target.value; }
-   updatevalid($event) { this.instructor.valid = $event.target.value; }
+    this.instructorService.postNewInstructor(instr).subscribe(Instructor => {
+      console.log("POST SUCCEEDED");
+    });
+  }
+  updateFirstName($event, i: number) { this.instructorList[i].firstName = $event.target.value; this.saveUpdatedInstructor(this.instructorList[i]) }
+  updateLastName($event, i: number) { this.instructorList[i].lastName = $event.target.value;this.saveUpdatedInstructor(this.instructorList[i]) }
+  updateEmail($event, i: number) { this.instructorList[i].email = $event.target.value; this.saveUpdatedInstructor(this.instructorList[i])}
+  updateID($event) { this.instructor.id = $event.target.value; }
+  updatevalid($event) { this.instructor.valid = $event.target.value; }
 
 
-   showAllInstructors(){
-      var element = document.getElementById("instructorlist").appendChild(document.createElement("article"));
-      console.log("Element = " + element);
-   }
-     getInstructorList() {
+  showAllInstructors() {
+    var element = document.getElementById("instructorlist").appendChild(document.createElement("article"));
+    console.log("Element = " + element);
+  }
+  getInstructorList() {
     this.instructorService.getInstructors().subscribe(instructors => {
       this.instructorList = instructors;
       console.log(this.instructorList.length);
-    }); 
+    });
   }
-  }
+}
 
 
