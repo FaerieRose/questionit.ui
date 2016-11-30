005c51907d26e3ee1de2428b1a26788eaf92ebb0
RemondKarst <remond.karst@gmail.com>
2016-11-30 16:07:54 +0100

    bind to instructor problems with hgetting the id's

--- src/app/instructor/bindinstructortoclass.component.ts
+++ src/app/instructor/bindinstructortoclass.component.ts
@@ -1,75 +1,73 @@
 /* ----------------------------------------------------------------------------------- */
-/* Author       : FaerieRose                                                           */
-/* Date created : 23 Nov 2016                                                          */
+/* Author       : Rik & Rémond                                                           */
+/* Date created : 30 Nov 2016                                                          */
 /* ----------------------------------------------------------------------------------- */
 import { Component, OnInit } from '@angular/core';
+import { GlobalService } from '../global.service';
 
 import { Instructor } from './instructor';
 import { InstructorService } from './instructor.service';
-import { GlobalService } from '../global.service';
+
 
 import { StudentClassService } from '../studentclasses/studentclass.service';
 import { StudentClass } from '../studentclasses/studentclass';
 import { StudentClassComponent } from '../studentclasses/studentclass.component';
 
-import { EnumLanguages } from '../enums';
-import { EnumExams } from '../enums';
+
 
 @Component({
   selector: 'my-bindinstructortoclass',
-  templateUrl: './bindinstructortoclass.component.html',
+  // templateUrl: './bindinstructortoclass.component.html',
+  templateUrl: 'bindinstructortoclass.component.html',
   styleUrls: ['instructor.component.css'],
-  providers: [InstructorService, StudentClassService]
+  providers: [StudentClassService, InstructorService]
 })
 export class BindInstructorToClassComponent implements OnInit {
-  instructor: Instructor;
-  instructors = [];
-  languages = [];
-  exams = [];
-  instructorList: Instructor[];
-  studentclass: StudentClass;
-  studentClassList: StudentClass[];
-  list = { "instructor": Instructor[0] }
+  instructor: Instructor; // Create 1 instance of instructor
+  instructors = []; // Create a Array of instructors
+  instructorList: Instructor[]; //Create variable to hold all instructors Used in the HTML used in the ngFor list
+  studentclass: StudentClass; //Create 1 instance of StudentClass
+  studentClassList: StudentClass[]; // Create variable to hold all studentClasses Used in the HTML used in the ngFor list
+  list = { "instructor": Instructor[0] } // Create variable list with objects of instructor
+  classList = { "studentclass": StudentClass[0] } // Create variable list with objects of studentclass
 
 
   constructor(
-    private instructorService: InstructorService,
-    private studentClassService: StudentClassService,
-    private globalService: GlobalService) {
-    this.languages = this.globalService.getLanguages();
-    this.exams = this.globalService.getExams();
+    private instructorService: InstructorService, // Needed to access the methods for instructor
+    private studentClassService: StudentClassService,// Needed to access the methods for studentclass
+    private globalService: GlobalService) {// Needed to access the methods for globalService
   }
 
-  ngOnInit() {
-    this.getInstructorList();
-    this.studentClassService.getStudentClasses().subscribe(studentClasses => {
-      this.studentClassList = studentClasses;
+  ngOnInit() { // Like the name says this method will be executed on page load
+    this.instructorService.getInstructorsForClass(this.list.instructor).subscribe(instructors => { // Go to the instructorService and ask method getInstructorsForClass to give all instructors
+      this.instructorList = instructors; // In the locale variable instructorList place the outcome of getInstructorsForClass
+    });
+    this.studentClassService.getStudentClasses().subscribe(studentClasses => { // Same as above but then for studentclasses
+      this.studentClassList = studentClasses;  // Same as above but then for studentclasses
     });
   }
 
+  updateInstructor($event) { this.list.instructor = $event.target.value; } // Saves instructor on update
+  updateStudentClass($event) { this.classList.studentclass = $event.target.value; } //Saves studentclass on update
 
-  //onderstaande weg en net als boven interpreteren
-  getInstructorList() {
-    this.instructorService.getInstructorsForClass(this.list.instructor).subscribe(instructors => {
-      this.instructorList = instructors;
-    });
-  }
+  saveInstructorToClass() { //Save method for saving instructor in an studentclass and posting it in the database
+    console.log(" IN saveInstructorToClass "); // Just for checking and following
+    let instr = new Instructor();
+    instr.firstName = "test";
+    this.instructor ;
+    let studcl = this.classList.studentclass;
+    console.log("We zijn een stap verder.");
+
+    console.log("in sinstr zit :" + instr);
+    console.log("in studcl zit : " + studcl);
+
+    //  this.studentClassService.postInstructorToStudentClass(studcl, instr.id)
 
 
-  updateInstructor($event) { this.list.instructor = $event.target.value; }
 
-  saveInstructorToClass() {
-    console.log(" IN saveInstructorToClass ");
-    let instr = this.instructor;
-    let studcl = this.studentclass;
-
-  //  this.studentClassService.postInstructorToStudentClass(studcl, instr.id)
-
-  
-
-    this.instructorService.postInstructorId(this.instructor.id).subscribe(instructorId => {
-      console.log(" IN 2 saveInstructorToClass ");
-      if (instructorId > 0) {
+    this.instructorService.postNewInstructor(instr).subscribe(instructor=> {
+      console.log(" IN 2 saveInstructorToClass "); // Just for checking and following
+      if (instructor.id > 0) {
         console.log(" IN 3 saveInstructorToClass ");
         this.studentClassService.postInstructorToStudentClass(studcl, instr.id).subscribe(studentclass => {
           console.log("POST INSTRUCTOR ADDING TO CLASS SUCCEEDED");
