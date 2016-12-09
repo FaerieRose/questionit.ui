134fcd806dc44196f60f2329a59d572fcd2b75af
Bas <bas.smulders@gmail.com>
2016-12-09 09:28:06 +0100

    Complete version of 'Show Question'

--- src/app/question/show-question.component.ts
+++ src/app/question/show-question.component.ts
@@ -15,19 +15,22 @@ import { AnswerListService }        from '../answerlist/answerlist.service';
 @Component({
   selector: 'my-show-question',
   templateUrl: 'show-question.component.html',
-  styleUrls: [ 'question.component.css' ],
+  styleUrls: [ 'show-question.component.css' ],
   providers: [ QuestionService, AnswerListService ]
 })
 export class ShowQuestionComponent implements OnInit {
   question: Question;
   possibleAnswers: string[] = [ "" ];
   correctAnswers: AnswerList;
+  givenAnswers: AnswerList;
 
   constructor(
         private questionService  : QuestionService,
         private answerListService: AnswerListService, 
         private globalService    : GlobalService) {
     this.correctAnswers = this.resetCorrectAnswers();
+    this.givenAnswers = this.resetGivenAnswers();
+    
   }
 
   ngOnInit() {
@@ -41,6 +44,12 @@ export class ShowQuestionComponent implements OnInit {
     return correctAnswers;
   }
 
+  resetGivenAnswers(): AnswerList {
+    let givenAnswers = new AnswerList();
+    givenAnswers.id = -1;
+    givenAnswers.answers = [ false, false, false, false, false, false, false, false, false, false ];
+    return givenAnswers;
+  }
   resetPossibleAnswers() {
     while (this.possibleAnswers.length > 0) {
       this.possibleAnswers.pop();
@@ -77,11 +86,11 @@ export class ShowQuestionComponent implements OnInit {
     this.possibleAnswers.push("");
   }
 
-  updateLanguage($event)    { this.question.programmingLanguage  = $event.target.value; }
-  updateExam($event)        { this.question.forExam              = $event.target.value; }
+  //updateLanguage($event)    { this.question.programmingLanguage  = $event.target.value; }
+  //updateExam($event)        { this.question.forExam              = $event.target.value; }
   updateName($event)        { this.question.name                 = $event.target.value; }
   updateType($event)        { this.question.typeOfQuestion       = $event.target.value; }
-  updateExplanation($event) { this.question.explanationAnswer    = $event.target.value; }
+  //updateExplanation($event) { this.question.explanationAnswer    = $event.target.value; }
   updateQuestion($event)    { this.question.question             = $event.target.value; }
   updateAnswer(id: number, $event) {
     this.possibleAnswers[id] =  $event.target.value;
@@ -91,7 +100,6 @@ export class ShowQuestionComponent implements OnInit {
     this.correctAnswers.answers[id] = $event.target.checked;
     this.question.correctAnswers = this.correctAnswers;
   }
-
 
   saveQuestion() {
     let qstn = this.question;
@@ -103,4 +111,16 @@ export class ShowQuestionComponent implements OnInit {
       }
     });
   }
+  
+  saveGivenAnswers() {
+    let qstn = this.question;
+    this.answerListService.postAnswerList(this.correctAnswers).subscribe(answerListId => {
+      if (answerListId > 0) {
+        this.questionService.postNewQuestion(qstn, answerListId).subscribe(question => {
+          console.log("POST SUCCEEDED");
+        });
+      }
+    });
+  }
+
 }
