a54633c9aebc7e1ded6f6446a38eae84d822eef7
S-Martens <s.martens.ehv@gmail.com>
2016-12-28 11:06:47 +0100

    implement global service in postattempt

--- src/app/student/studentpostattempt.component.ts
+++ src/app/student/studentpostattempt.component.ts
@@ -2,13 +2,14 @@
 /* Author       : S.Martens                                                            */
 /* Date created : 27 Dec 2016                                                          */
 /* ----------------------------------------------------------------------------------- */
-import { Component, OnInit }  from '@angular/core';
+import { Component, OnInit }      from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { Attempt }                 from '../attempt/attempt';
-import { Student }                 from './student';
-import { AttemptService }        from '../attempt/attempt.service';
-import { StudentService }        from '../student/student.service';
-import { Router }                from '@angular/router';
+import { Attempt }                from '../attempt/attempt';
+import { Student }                from './student';
+import { AttemptService }         from '../attempt/attempt.service';
+import { StudentService }         from '../student/student.service';
+import { Router }                 from '@angular/router';
+import { GlobalService }          from '../global.service';
 
 @Component({
   selector: 'my-studentpostattempt',
@@ -22,7 +23,9 @@ export class StudentPostAttemptComponent implements OnInit {
   constructor(
         private route: ActivatedRoute,
         private attemptService  : AttemptService,
-        private router: Router
+        private studentService : StudentService,
+        private router: Router,
+        private globalService: GlobalService
   ) { }
 
   ngOnInit() {
@@ -30,11 +33,21 @@ export class StudentPostAttemptComponent implements OnInit {
   }
 
   studentFinishAttempt() : void{
-      this.router.navigate(['attemptscore/:1']);
+    var attempt_id : Number = 1;
+   // this.globalService.
+    this.EndAttempt(attempt_id);  
+    this.router.navigate(['attemptscore/'+ attempt_id]);
   }
   
   studentBackContinue(){
-      this.router.navigate(['/question/show']);
+    this.router.navigate(['/question/show']);
   }  
+  
+  EndAttempt(attempt_id) {
+    this.studentService.postEndAttempt(attempt_id).subscribe(Student => {
+      console.log("POST end attempt made");
+      }); 
+  }
+  
 
 }
