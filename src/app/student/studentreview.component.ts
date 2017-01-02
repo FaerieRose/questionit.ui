37a44729f97d2d59b07f8d704fa318cd4e96914d
schave76@gmail.com <schave76@gmail.com>
2017-01-02 18:16:45 +0100

    back button...

--- src/app/student/studentreview.component.ts
+++ src/app/student/studentreview.component.ts
@@ -3,12 +3,14 @@
 /* Date created : 19 Dec 2016                                                          */
 /* ----------------------------------------------------------------------------------- */
 import { Component, OnInit }      from '@angular/core';
-//import { ActivatedRoute, Params } from '@angular/router';
+import { Router }                 from '@angular/router';
 import { Attempt }                from '../attempt/attempt';
 import { Student }                from './student';
 import { GlobalService }          from '../global.service';
 import { AttemptService }         from '../attempt/attempt.service';
 import { StudentService }         from '../student/student.service';
+import { EnumLanguages }          from '../enums'; 
+
 
 @Component({
   selector: 'my-studentreview',
@@ -22,12 +24,14 @@ export class StudentReviewComponent implements OnInit {
   currentStudent = null;
   currentStudentAttempts = [];
   attemptStartDates = [];           //: string[]; NOPE...defining array type makes field undefined...?!?!
+  attemptLanguages = [];
   currentStudentScores = [];
 
   constructor(
         private globalService: GlobalService,
         private attemptService  : AttemptService,
-        private studentService  : StudentService
+        private studentService  : StudentService,
+        private router: Router
   ) { }
 
   ngOnInit() {
@@ -35,13 +39,16 @@ export class StudentReviewComponent implements OnInit {
     this.studentService.getStudentById(this.globalService.getStudentID()).subscribe(student =>{
       console.log(JSON.stringify(student));
       this.currentStudentAttempts = student.attempts;
-      this.attemptStartDates.length = 1;
-      this.currentStudentScores.length = 1;
+      this.attemptStartDates.length = 0;
+      this.currentStudentScores.length = this.currentStudentAttempts.length;
+      this.attemptLanguages.length = 0;
       for (var i = 0; i < this.currentStudentAttempts.length; i++){
         this.attemptStartDates.push((new Date(this.currentStudentAttempts[i].startDateTime)).toLocaleString());
-        //this probably runs into async probls...needs fix
+        this.attemptLanguages.push(EnumLanguages[this.currentStudentAttempts[i].testTemplate.programmingLanguage]);
+        //TODO this runs into async probls...
         this.attemptService.getScoresRate(this.currentStudentAttempts[i].id).subscribe(score =>{
           this.currentStudentScores[i] = score;
+          console.log("ngoninit. i: " + i + " crntscr[i]: " + this.currentStudentScores[i] + " score: " + score);
         });
       }
       this.currentStudent = student;
@@ -49,5 +56,8 @@ export class StudentReviewComponent implements OnInit {
     });
   }
 
+  goBack(){
+    this.router.navigate(['studentlogin']);
+  }
 
 }
