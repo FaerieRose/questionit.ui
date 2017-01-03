6a0c4a3a1885fbab5af26a750e0961bdaddbcad0
Bas <bas.smulders@gmail.com>
2017-01-03 21:58:22 +0100

    Removed some commented code from attempt.module.ts

--- src/app/attempt/attempt.module.ts
+++ src/app/attempt/attempt.module.ts
@@ -2,26 +2,6 @@
 /* Author       : Bas Smulders                                                         */
 /* Date created : 09 Dec 2016                                                          */
 /* ----------------------------------------------------------------------------------- */
-// import { NgModule }            from '@angular/core';
-// import { CommonModule }        from '@angular/common';
-
-// import { QuestionComponent }            from '../question/question.component';
-// import { QuestionsComponent }           from '../question/questions.component';
-// import { ShowQuestionComponent }        from '../question/show-question.component';
-// import { QuestionDisplayComponent }     from '../question/question-display.component';
-
-// import { QuestionRoutingModule }        from '../question/question-routing.module';
-
-// @NgModule({
-//   imports: [ CommonModule, QuestionRoutingModule ],
-//   declarations: [
-//     QuestionComponent, 
-//     QuestionsComponent, 
-//     ShowQuestionComponent,
-//     QuestionDisplayComponent
-//   ]
-// })
-
 import { NgModule }            from '@angular/core';
 import { CommonModule }        from '@angular/common';
 
