72ba7ee59ea571c4631c457bf622ffdfa17b629c
schave76@gmail.com <schave76@gmail.com>
2017-01-06 10:58:55 +0100

    rebuilt createtest. should work. not tested yet

--- src/app/createtest/createtest.component.ts
+++ src/app/createtest/createtest.component.ts
@@ -1,10 +1,11 @@
 /* ----------------------------------------------------------------------------------- */
 /* Author       : Rik & Rémond                                                         */
 /* Date created : 15 Dec 2016                                                          */
+/* Rebuilt      : 06-01-2017, Dave Schellekens
 /* ----------------------------------------------------------------------------------- */
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
-import { Router } from '@angular/router';
+//import { ActivatedRoute, Params } from '@angular/router';
+//import { Router } from '@angular/router';
 
 import { GlobalService } from '../global.service';
 import { TestTemplate } from '../testtemplate/testtemplate';
@@ -28,16 +29,18 @@ export class CreateTestComponent implements OnInit {
   exams = [];
   testTemplate: TestTemplate;
   questionList: Question[];
+  includeInTest: Boolean[];
   testTemplateList: TestTemplate[];
-  list = { "exam": EnumExams[0], "language": EnumLanguages[0], "enabled": true, "obsolete": false }
+  questionListFilter = { "exam": EnumExams[0], "language": EnumLanguages[0], "enabled": true }
 
   constructor(
     private questionService: QuestionService,
     private testTemplateService: TestTemplateService,
     private globalService: GlobalService,
-    private route: ActivatedRoute,
-    private router: Router) {
-    this.testTemplate = new TestTemplate();
+    //private route: ActivatedRoute,
+    //private router: Router
+    ) {
+    //this.testTemplate = new TestTemplate();
     this.languages = this.globalService.getLanguages();
     this.exams.push({ "id": 0, "name": "NONE" });
   }
@@ -45,93 +48,104 @@ export class CreateTestComponent implements OnInit {
   ngOnInit() {
     //  let id = +this.route.snapshot.params['id'];
     // console.log("===---HET ID---==="+id);
-    this.getTestTemplate(0);
+    //this.getTestTemplate(0);
+    this.testTemplate = new TestTemplate();
+    this.testTemplate.attemptTimeInMinutes = 0;
+    this.testTemplate.forExam = 0;
+    this.testTemplate.isEnabled = true;
+    this.testTemplate.name = "";
+    this.testTemplate.programmingLanguage = 0;
+    this.testTemplate.questions = null;
+      
+    
     this.getQuestionList();
 
     //  this.getTestTemplateById(1);
-    this.testTemplate = new TestTemplate();
+    //this.testTemplate = new TestTemplate();
 
   }
 
-  getTestTemplate(id: number) {
-    this.testTemplate = null;
-    if (id == 0) {
-      this.testTemplate = new TestTemplate();
-      this.testTemplate.attemptTimeInMinutes = 0;
-      this.testTemplate.forExam = 0;
-      this.testTemplate.isEnabled = true;
-      this.testTemplate.name = "";
-      this.testTemplate.programmingLanguage = 0;
-      this.testTemplate.questions = null;
-      //this.testTemplate.size = 0;
-      this.saveTest();
+  // getTestTemplate(id: number) {
+  //     this.testTemplateService.getTestTemplateById(id).subscribe(testTemplate => {
+  //       this.testTemplate = testTemplate;
+  //     });
+  //   }
+  // }
 
-    } else {
-      this.testTemplateService.getTestTemplateById(id).subscribe(testTemplate => {
-        this.testTemplate = testTemplate;
-
-
-      });
-    }
-  }
-
-
-
-  getTestTemplateById(id: number) {
-
-    this.testTemplateService.getTestTemplateById(id).subscribe(testTemplate => {
-      if (testTemplate.id == 1) {
-        this.testTemplate = new TestTemplate();
-      } else {
-        this.testTemplate = testTemplate;
-      }
-    });
-  }
+  // getTestTemplateById(id: number) {
+  //   this.testTemplateService.getTestTemplateById(id).subscribe(testTemplate => {
+  //     if (testTemplate.id == 1) {
+  //       this.testTemplate = new TestTemplate();
+  //     } else {
+  //       this.testTemplate = testTemplate;
+  //     }
+  //   });
+  // }
 
   getQuestionList() {
-    this.questionService.getQuestions(this.list.exam, this.list.language, this.list.enabled).subscribe(questions => {
+    this.questionService.getQuestions(this.questionListFilter.exam, this.questionListFilter.language, this.questionListFilter.enabled).subscribe(questions => {
       this.questionList = questions;
       console.log(this.questionList.length);
+      this.includeInTest = null;
+      for (var i = 0; i < this.questionList.length; i++) { this.includeInTest.push(false); }
     });
   }
 
   updateLanguage($event) {
-    this.list.language = EnumLanguages[parseInt($event.target.value)];
+    this.questionListFilter.language = EnumLanguages[parseInt($event.target.value)];
     this.testTemplate.programmingLanguage = parseInt($event.target.value);
-    this.questionService.getLevels(this.list.language).subscribe(levels => {
+    this.questionService.getLevels(this.questionListFilter.language).subscribe(levels => {
       console.log(levels);
       this.exams.length = 1;
       for (let i = 0; i < levels.length; i++) {
         this.exams.push({ "id": i + 1, "name": levels[i] });
       }
     })
-    this.list.exam = this.exams[0].name;
+    this.questionListFilter.exam = this.exams[0].name;
     this.getQuestionList();
   }
 
-  addOrRemoveQuestionFromTest($event, questionId: number) {
+  addOrRemoveQuestionFromTest($event, questionIndex: number) {
     console.log ("de waarde van checked :" + $event.target.checked);
-    if($event.target.checked){
-      console.log("*********************************************************");
-      this.testTemplateService.addQuestionToTemplate(this.testTemplate.id, questionId).subscribe(q => { });
-    } else {
-      console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-      this.testTemplateService.removeQuestionToTemplate(this.testTemplate.id, questionId).subscribe(q => { });
-    }
+    this.includeInTest[questionIndex] = $event.target.checked;
+    // if($event.target.checked){
+    //   console.log("*********************************************************");
+    //   this.testTemplateService.addQuestionToTemplate(this.testTemplate.id, questionId).subscribe(q => { });
+    // } else {
+    //   console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+    //   this.testTemplateService.removeQuestionToTemplate(this.testTemplate.id, questionId).subscribe(q => { });
+    // }
   }
 
   saveTest() {
     let template = this.testTemplate;
-    if (template.id == -1) { this.testTemplate.id = 1; }
-    this.testTemplateService.postNewTestTemplate(template).subscribe(TestTemplate => {
-      this.testTemplate.id = TestTemplate.id;
+    // ???? remnant of new/existing??? if (template.id == -1) { this.testTemplate.id = 1; }
+    //cannot post template with list of questionIDs, only list of Questions.:(
+    //this is probably the reason why questions were added/removed on checkbox click.  
+    //TODO create backend call to post template together with list of questionIDs?  
+    //post template first  
+    this.testTemplateService.postNewTestTemplate(template).subscribe(tt => {
+      this.testTemplate.id = tt.id;
+      //TODO: result?
+      for (var i = 0; i < this.questionList.length; i++){
+          if (this.includeInTest[i] == true) {
+            this.testTemplateService.addQuestionToTemplate(this.testTemplate.id, this.questionList[i].id).subscribe(q => { });
+          }
+      }
     });
   }
-  updateTestName($event) { this.testTemplate.name = $event.target.value; }
+
+  updateTestName($event) {
+     this.testTemplate.name = $event.target.value;
+  }
+
   updateExam($event) {
-    this.list.exam = EnumExams[parseInt($event.target.value)]; this.testTemplate.forExam = parseInt($event.target.value);
+    this.questionListFilter.exam = EnumExams[parseInt($event.target.value)]; this.testTemplate.forExam = parseInt($event.target.value);
     this.getQuestionList();
   }
-  updateDuration($event) { this.testTemplate.attemptTimeInMinutes = $event.target.value; }
+  
+  updateDuration($event) {
+     this.testTemplate.attemptTimeInMinutes = $event.target.value;
+  }
 
 }
